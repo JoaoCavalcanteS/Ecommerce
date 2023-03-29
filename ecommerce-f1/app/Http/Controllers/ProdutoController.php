822ea5edaf40d6b8f98e1afb44427d6a3cc5b979
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2023-03-29 00:57:17 -0300

    Add: Routes and Controllers

---
+++ ecommerce-f1/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index(){
+        $produtos = Produto::all();//retorna todos os produtos e guarda nessa variavel
+
+      return view('produto.index')->with('produtos',$produtos);//retorna a view numa pasta(n pode ser no plural, pois é o q está na model) e o arquivo("".blade.php)
+    }
+
+    public function show(Produto $produto){ // model e variavel
+     // dd($produto);
+     // retorna só um produto
+      //return view('produto.show')->with('produto',$produto);
+      $maisProdutos = Categoria::find($produto->CATEGORIA_ID)->Produtos;
+      return view ('produto.show', ['produto' =>$produto,'maisProdutos' => $maisProdutos]);
+
+    }
+
+}
--- ecommerce-f1/routes/web.php
+++ ecommerce-f1/routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/produto',[ProdutoController::class,'index']);// controllee-class-funcão dentro da controller(site)
+Route::get('/produto/{produto}',[ProdutoController::class,'show'])->name('produto.show');//aqui , no servidor, será produto/1
