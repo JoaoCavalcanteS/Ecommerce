0c0c0741e749b19a8c1a1a0458684eba5ca6b886
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2023-04-04 22:09:53 -0300

    Conexão com o banco de dados
    Atento a mudar a conexão do banco de dados e criar a tabela de imagem

---
+++ ecommerce-f1/app/Models/Produto.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+    protected $table ='PRODUTO';  //fazer com que ele reconheça a tabela do banco e trocar o nome da tabela
+    protected $primaryKey ='produtoId';
+
+    // public function ProdutoImagem(){
+    //     return $this->hasMany(ProdutoImagem::class,'produtoId','produtoId');//as colunas q se ligam
+    //     // belongs to many é tipo uma categoria pertence para varios produtos
+    //     //hasMany varias imagens para um produto
+    // }
+
+    public function ProdutoEstoque(){
+        return $this->hasOne(ProdutoEstoque::class,'produtoId','produtoId');
+
+    }
+}
